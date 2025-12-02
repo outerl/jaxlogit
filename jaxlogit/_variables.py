c7e6016fc4a671a75966336d9525f32343bbc0c8
Tyler Pearn <tyler@outerloop.io>
2025-12-02 17:06:10 +1000

    Add parameters dataclass

--- jaxlogit/_variables.py
+++ jaxlogit/_variables.py
@@ -1,26 +1,40 @@
-class Variables:
-    """
-    A grouping of commonly used variables
+from dataclasses import dataclass
+import jax.numpy as jnp
+
+
+@dataclass
+class Parameters:
+    """Parameters of the distributions for coefficients of explanatory variables.
 
     #TODO: see whether any functions can be abstracted here
     #TODO: docstring description of each
     #TODO: add class logic
     """
-    def __init__(self, Xdf, Xdr, draws, mask, values_for_mask, mask_chol, values_for_chol_mask,
-                 rand_idx_norm, rand_idx_truncnorm, draws_idx_norm, draws_idx_truncnorm, 
-                 fixed_idx, idx_ln_dist, rand_idx_stddev, rand_idx_chol):
-        self.Xdf = Xdf
-        self.Xdr = Xdr
-        self.draws = draws
-        self.mask = mask
-        self.values_for_mask = values_for_mask
-        self.mask_chol = mask_chol
-        self.values_for_chol_mask = values_for_chol_mask
-        self.rand_idx_norm = rand_idx_norm
-        self.rand_idx_truncnorm = rand_idx_truncnorm
-        self.draws_idx_norm = draws_idx_norm
-        self.draws_idx_truncnorm = draws_idx_truncnorm
-        self.fixed_idx = fixed_idx
-        self.idx_ln_dist = idx_ln_dist
-        self.rand_idx_stddev = rand_idx_stddev
-        self.rand_idx_chol = rand_idx_chol
+
+    Xdf: jnp.ndarray
+    Xdr: jnp.ndarray
+    draws: jnp.ndarray
+    mask: jnp.ndarray
+    values_for_mask: jnp.ndarray
+    mask_chol: jnp.ndarray
+    values_for_chol_mask: jnp.ndarray
+    rand_idx_norm: jnp.ndarray
+    rand_idx_truncnorm: jnp.ndarray
+    draws_idx_norm: jnp.ndarray
+    draws_idx_truncnorm: jnp.ndarray
+    fixed_idx: jnp.ndarray
+    idx_ln_dist: jnp.ndarray
+    rand_idx_stddev: jnp.ndarray
+    rand_idx_chol: jnp.ndarray
+
+
+@dataclass
+class InventoryItem:
+    """Class for keeping track of an item in inventory."""
+
+    name: str
+    unit_price: float
+    quantity_on_hand: int = 0
+
+    def total_cost(self) -> float:
+        return self.unit_price * self.quantity_on_hand
