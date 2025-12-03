023b994bd09863f49ef16d9b50d7744689fd224f
Evelyn Bond <evelyn@outerloop.io>
2025-12-03 10:42:47 +1000

    smoke test for gradient

--- tests/test__optimize.py
+++ tests/test__optimize.py
@@ -1,4 +1,4 @@
-from jaxlogit._optimize import hessian
+from jaxlogit._optimize import hessian, gradient
 import numpy as np
 import pandas as pd
 import jax as jax
@@ -6,8 +6,8 @@ import jax.numpy as jnp
 import pytest
 
 
+# TODO: test hessian input validation
 def test_hessian_no_finite_diff():
-    # x is the betas
     def test_function(x, a, b, c, force_positive_chol_diag, num_panels):
         return a ** x[0] + b ** x[1] + a / c + x[2] ** 5
 
@@ -35,4 +35,20 @@ def test_hessian_finite_diff():
     args = (a, b, c, fpcd, num_panels)
     x = jnp.array([0.1, 0.1, 0.1])
     expected = np.array([np.array([3.0426044, 0.0, 0.0]), np.array([0.0, 0.5149368, 0.0]), np.array([0.0, 0.0, 0.02])])
-    assert expected == pytest.approx(hessian(test_function, x, False, True, *args), rel=6e-2)  # finite_diff
+    assert expected == pytest.approx(hessian(test_function, x, False, True, *args), rel=6e-2)
+
+
+def test_gradient():
+    def test_function(x, a, b, c):
+        return a ** x[0] + b ** x[1] + a / c + x[2] ** 5
+
+    a = 5.0
+    b = 2.0
+    c = 3.0
+    args = (a, b, c)
+    x = jnp.array([0.1, 0.1, 0.1])
+
+    expected = jnp.zeros((1, 3))
+    expected = expected.at[0, 0].set(1.9073486)
+    expected = expected.at[0, 1].set(0.715255)
+    assert expected == pytest.approx(gradient(test_function, x, *args))
