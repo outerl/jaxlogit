d5da1da303fafb12aafb78406098aadca34fecad
Evelyn Bond <evelyn@outerloop.io>
2025-12-04 11:17:55 +1000

    fix style issues and remove file from another branch

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,30 +5,36 @@ from jaxlogit.utils import lrtest
 from jaxlogit.mixed_logit import MixedLogit
 
 
-dfw = pd.DataFrame({'id': [1, 2, 3, 4, 5],
-                    'time_car': [1, 1, 1, 1, 1],
-                    'time_bus': [2, 2, 2, 2, 2],
-                    'cost_bus': [3, 3, 3, 3, 3],
-                    'income': [9, 8, 7, 6, 5],
-                    'age': [.6, .5, .4, .3, .2],
-                    'y': ['bus', 'bus', 'bus', 'car', 'car']})
+dfw = pd.DataFrame(
+    {
+        "id": [1, 2, 3, 4, 5],
+        "time_car": [1, 1, 1, 1, 1],
+        "time_bus": [2, 2, 2, 2, 2],
+        "cost_bus": [3, 3, 3, 3, 3],
+        "income": [9, 8, 7, 6, 5],
+        "age": [0.6, 0.5, 0.4, 0.3, 0.2],
+        "y": ["bus", "bus", "bus", "car", "car"],
+    }
+)
+
 
 def test_wide_to_long():
     """
     Ensures a pandas dataframe is properly converted from wide to long format
     Adapted from xlogit tests
     """
-    expec = pd.DataFrame({'id': [1, 1, 2, 2, 3, 3, 4, 4, 5, 5],
-                          'alt': ['car', 'bus', 'car', 'bus', 'car', 'bus',
-                                  'car', 'bus', 'car', 'bus'],
-                          'time': [1, 2, 1, 2, 1, 2, 1, 2, 1, 2],
-                          'cost': [0, 3, 0, 3, 0, 3, 0, 3, 0, 3],
-                          'income': [9, 9, 8, 8, 7, 7, 6, 6, 5, 5],
-                          'age': [.6, .6, .5, .5, .4, .4, .3, .3, .2, .2],
-                          'y': ['bus', 'bus', 'bus', 'bus', 'bus', 'bus',
-                                'car', 'car', 'car', 'car']})
-    dfl = wide_to_long(dfw, id_col="id", alt_list=["car", "bus"],
-                       alt_name="alt", varying=["time", "cost"], empty_val=0)
+    expec = pd.DataFrame(
+        {
+            "id": [1, 1, 2, 2, 3, 3, 4, 4, 5, 5],
+            "alt": ["car", "bus", "car", "bus", "car", "bus", "car", "bus", "car", "bus"],
+            "time": [1, 2, 1, 2, 1, 2, 1, 2, 1, 2],
+            "cost": [0, 3, 0, 3, 0, 3, 0, 3, 0, 3],
+            "income": [9, 9, 8, 8, 7, 7, 6, 6, 5, 5],
+            "age": [0.6, 0.6, 0.5, 0.5, 0.4, 0.4, 0.3, 0.3, 0.2, 0.2],
+            "y": ["bus", "bus", "bus", "bus", "bus", "bus", "car", "car", "car", "car"],
+        }
+    )
+    dfl = wide_to_long(dfw, id_col="id", alt_list=["car", "bus"], alt_name="alt", varying=["time", "cost"], empty_val=0)
     assert dfl.equals(expec)
 
 
@@ -39,7 +45,6 @@ def test_lrtest():
     Adapted from xlogit tests
     """
     general = MixedLogit()
-    general.loglikelihood = 1312
     restricted = MixedLogit()
     restricted.loglikelihood = -1305
     general.loglikelihood = -1312
@@ -47,6 +52,6 @@ def test_lrtest():
     restricted.coeff_ = np.zeros(2)
 
     obtained = lrtest(general, restricted)
-    expected = {'pval': 0.0009118819655545164, 'chisq': 14, 'degfree': 2}
+    expected = {"pval": 0.0009118819655545164, "chisq": 14, "degfree": 2}
     for key in obtained:
         assert obtained[key] == expected[key]
