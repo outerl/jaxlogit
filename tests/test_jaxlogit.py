3ec267bdb8a98060aa8b52402f4c38f5778949eb
TylerP132 <126933196+TylerP132@users.noreply.github.com>
2025-12-12 16:59:48 +1000

    Add tests to Mixed Logit (#44)
    Co-authored-by: Tyler Pearn <tyler@outerloop.io>

--- tests/test_jaxlogit.py
+++ tests/test_jaxlogit.py
@@ -4,6 +4,7 @@ import numpy as np
 import jax
 import jax.numpy as jnp
 import pickle
+import regex as re
 
 from jaxlogit._variables import ParametersSetup
 from jaxlogit._config_data import ConfigData
@@ -22,7 +23,7 @@ SEED = 0
 def make_simple_data():
     N = 6  # individuals
     J = 3  # alternatives
-    K = 2  # variables
+    K = 3  # variables
     np.random.seed(SEED)
     X = np.random.randn(N * J, K)
     # y = np.random.randint(0, 2, size=(N * J,))
@@ -43,28 +44,120 @@ def simple_data():
     return make_simple_data()
 
 
-def test_mixed_logit_fit(simple_data):
+def test_bad_random_variables(simple_data):
     X, y, ids, alts, avail, panels, weights = simple_data
-
     varnames = [f"x{i}" for i in range(X.shape[1])]
 
     model = MixedLogit()
-    randvars = {varnames[0]: "n"}
-    fixedvars = {}
+    randvars = {varnames[0]: "n", varnames[1]: "fake"}
     config = ConfigData(
         avail=avail,
         panels=panels,
         weights=weights,
         n_draws=3,
-        fixedvars=fixedvars,
         optim_method="L-BFGS-B",
         init_coeff=None,
         skip_std_errs=True,
     )
-    result = model.fit(X, y, varnames, alts, ids, randvars, config)
+    with pytest.raises(
+        ValueError, match="Wrong mixing distribution in 'randvars'. Accepted distrubtions are n, ln, t, u, tn"
+    ):
+        model.fit(X, y, varnames, alts, ids, randvars, config)
 
+
+def test_mixed_logit_fit_different_variables(simple_data):
+    X, y, ids, alts, avail, panels, weights = simple_data
+    rand_var_type_combos = [
+        ("n", "n", "n"),
+        ("n", "ln", "ln"),
+        ("n_trunc", "n", "ln"),
+        ("n_trunc", "n_trunc", "n_trunc"),
+        ("n_trunc", None, None),
+        (None, None, None),
+        ("n", None, None),
+        ("n", "ln", None),
+    ]
+    varnames = [f"x{i}" for i in range(X.shape[1])]
+
+    for include_correlations in [True, False]:
+        for rand_var_types in rand_var_type_combos:
+            number_normal_and_lognormal = rand_var_types.count("n") + rand_var_types.count("ln")
+
+            model = MixedLogit()
+            randvars = {varnames[i]: rand_var_types[i] for i in range(len(rand_var_types)) if rand_var_types[i]}
+            set_vars = {}
+            config = ConfigData(
+                avail=avail,
+                panels=panels,
+                weights=weights,
+                n_draws=3,
+                set_vars=set_vars,
+                optim_method="L-BFGS-B",
+                init_coeff=None,
+                include_correlations=include_correlations,
+                skip_std_errs=True,
+            )
+            if include_correlations and number_normal_and_lognormal < 2:
+                with pytest.raises(
+                    ValueError,
+                    match=re.escape(
+                        f"Only {number_normal_and_lognormal} normal based variable(s). Cannot use correlation"
+                    ),
+                ):
+                    result = model.fit(X, y, varnames, alts, ids, randvars, config)
+                continue
+
+            result = model.fit(X, y, varnames, alts, ids, randvars, config)
+
+            assert result is not None
+            assert "fun" in result
+            number_normal_and_lognormal = rand_var_types.count("n") + rand_var_types.count("ln")
+            assert (
+                len(result.x)
+                == 2 * len(rand_var_types)
+                - rand_var_types.count(None)
+                + number_normal_and_lognormal * (number_normal_and_lognormal - 1) / 2 * include_correlations
+            )
+
+
+def test_mixed_logit_fit_no_panels_weights(simple_data):
+    X, y, ids, alts, avail, panels, weights = simple_data
+
+    model = MixedLogit()
+    varnames = [f"x{i}" for i in range(X.shape[1])]
+    rand_var_types = ("n_trunc", "n", "ln")
+    randvars = {varnames[i]: rand_var_types[i] for i in range(len(rand_var_types)) if rand_var_types[i]}
+    set_vars = {}
+
+    no_weights_or_panel_config = ConfigData(
+        avail=avail, n_draws=3, set_vars=set_vars, optim_method="L-BFGS-B", init_coeff=None, skip_std_errs=False
+    )
+    result = model.fit(X, y, varnames, alts, ids, randvars, no_weights_or_panel_config)
     assert result is not None
     assert "fun" in result
+
+
+def test_mixed_logit_fit_set_variables(simple_data):
+    X, y, ids, alts, avail, panels, weights = simple_data
+    varnames = [f"x{i}" for i in range(X.shape[1])]
+
+    randvars = {varnames[0]: "n"}
+    set_vars = {varnames[1]: 0.0}
+    config = ConfigData(
+        avail=avail,
+        panels=panels,
+        weights=weights,
+        n_draws=3,
+        set_vars=set_vars,
+        optim_method="L-BFGS-B",
+        init_coeff=None,
+        skip_std_errs=True,
+    )
+    model = MixedLogit()
+    result = model.fit(X, y, varnames, alts, ids, randvars, config)
+    assert result is not None
+    assert "fun" in result
+    assert len(result.x) == 4  # two from normal distribution, two from un-parameterised variables
 
 
 @pytest.mark.skip(reason="different python versions and OSs give different results")
@@ -75,13 +168,13 @@ def test_mixed_logit_fit_against_previous_results(simple_data):
 
     model = MixedLogit()
     randvars = {varnames[0]: "n"}
-    fixedvars = {}
+    set_vars = {}
 
     config = ConfigData(
         skip_std_errs=True,
         init_coeff=None,
         optim_method="L-BFGS-B",
-        fixedvars=fixedvars,
+        set_vars=set_vars,
         n_draws=3,
         weights=weights,
         panels=panels,
@@ -117,13 +210,13 @@ def test_loglike_individual_and_total(simple_data):
     model = MixedLogit()
     randvars = {varnames[0]: "n"}
 
-    fixedvars = {}
+    set_vars = {}
     config = ConfigData(
         avail=np.array(df["avail"]),
         panels=np.array(df["person_id_contiguous"]),
         weights=np.array(df["weight"]),
         n_draws=3,
-        fixedvars=fixedvars,
+        set_vars=set_vars,
         init_coeff=None,
         include_correlations=False,
     )
@@ -155,13 +248,13 @@ def test_probability_individual(simple_data):
 
     model = MixedLogit()
     randvars = {varnames[0]: "n"}
-    fixedvars = {}
+    set_vars = {}
     config = ConfigData(
         avail=np.array(df["avail"]),
         panels=np.array(df["person_id_contiguous"]),
         weights=np.array(df["weight"]),
         n_draws=3,
-        fixedvars=fixedvars,
+        set_vars=set_vars,
         init_coeff=None,
         include_correlations=False,
     )
@@ -174,54 +267,51 @@ def test_probability_individual(simple_data):
     assert not jnp.any(jnp.isnan(probs))
 
 
-@pytest.mark.skip(reason="Not working properly")
 def test_transform_rand_betas_shapes():
-    Kr = 2
+    Kr = 3
     N = 4
     R = 3
     betas = jnp.arange(Kr + Kr + Kr * (Kr + 1) // 2, dtype=float)
     draws = jnp.ones((N, Kr, R))
-    rand_idx = jnp.arange(Kr)
-    sd_start_idx = Kr
-    sd_slice_size = Kr
-    chol_start_idx = sd_start_idx + sd_slice_size
-    chol_slice_size = (sd_slice_size * (sd_slice_size + 1)) // 2 - sd_slice_size
-    idx_ln_dist = jnp.array([], dtype=int)
-
-    parameter_info = ParametersSetup()
-
-    out = _transform_rand_betas(
-        betas, draws, rand_idx, sd_start_idx, sd_slice_size, chol_start_idx, chol_slice_size, idx_ln_dist, False
+    rvdist = np.array(["n", "ln", "n_trunc"])
+    rvidx = jnp.array([True, True, True])
+    rvidx_normal_bases = jnp.array([True, True, False])
+    rvidx_truncnorm_based = jnp.array([False, False, True])
+    coef_names = np.array(["normal_variable", "lognormal_variable", "truncated_normal_variable"])
+    config = ConfigData(include_correlations=False, force_positive_chol_diag=False)
+    parameter_info_no_correlation = ParametersSetup(
+        rvdist, rvidx, rvidx_normal_bases, rvidx_truncnorm_based, coef_names, betas, config
     )
+
+    config_corellation = ConfigData(include_correlations=True, force_positive_chol_diag=True)
+    parameter_info_correlation = ParametersSetup(
+        rvdist, rvidx, rvidx_normal_bases, rvidx_truncnorm_based, coef_names, betas, config_corellation
+    )
+
+    out = _transform_rand_betas(betas, False, draws, parameter_info_no_correlation)
     assert out.shape == (N, Kr, R)
-    out_corr = _transform_rand_betas(betas, True, draws, parameter_info)
+    out_corr = _transform_rand_betas(betas, True, draws, parameter_info_correlation)
     assert out_corr.shape == (N, Kr, R)
 
 
-@pytest.mark.skip(reason="Not working properly")
 def test_transform_rand_betas_jit():
     Kr = 2
     N = 3
     R = 2
     betas = jnp.arange(Kr + Kr + Kr * (Kr + 1) // 2, dtype=float)
     draws = jnp.ones((N, Kr, R))
-    rand_idx = jnp.arange(Kr)
-    sd_start_idx = Kr
-    sd_slice_size = Kr
-    chol_start_idx = sd_start_idx + sd_slice_size
-    chol_slice_size = (sd_slice_size * (sd_slice_size + 1)) // 2 - sd_slice_size
-    idx_ln_dist = jnp.array([], dtype=int)
-    fn = jax.jit(
-        _transform_rand_betas,
-        static_argnames=[
-            "sd_start_index",
-            "sd_slice_size",
-            "chol_start_idx",
-            "chol_slice_size",
-            "include_correlations",
-        ],
+    rvdist = np.array(["n", "n"])
+    rvidx = jnp.array([True, True])
+    rvidx_normal_bases = jnp.array([True, True])
+    rvidx_truncnorm_based = jnp.array([False, False])
+    coef_names = np.array(["testvar1", "testvar2"])
+
+    config_corellation = ConfigData(include_correlations=True, force_positive_chol_diag=True)
+    parameter_info_correlation = ParametersSetup(
+        rvdist, rvidx, rvidx_normal_bases, rvidx_truncnorm_based, coef_names, betas, config_corellation
     )
-    out = fn(betas, draws, rand_idx, sd_start_idx, sd_slice_size, chol_start_idx, chol_slice_size, idx_ln_dist, True)
+    fn = jax.jit(_transform_rand_betas, static_argnames=["parameter_info", "force_positive_chol_diag"])
+    out = fn(betas, True, draws, parameter_info_correlation)
     assert out.shape == (N, Kr, R)
 
 
@@ -232,13 +322,13 @@ def save_simple_data_output():
 
     model = MixedLogit()
     randvars = {varnames[0]: "n"}
-    fixedvars = {}
+    set_vars = {}
 
     config = ConfigData(
         skip_std_errs=True,
         init_coeff=None,
         optim_method="L-BFGS-B",
-        fixedvars=fixedvars,
+        set_vars=set_vars,
         n_draws=3,
         weights=weights,
         panels=panels,
