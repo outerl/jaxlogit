e85a5ab4e7d10cbc336f8fce03ac36a44c802f4b
Tyler Pearn <tyler@outerloop.io>
2025-12-15 11:00:33 +1000

    Add setup of Sphinx documentation

---
+++ docs/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'jaxlogit'
+copyright = '2025, Jan Zill, Tyler Pearn, Evelyn Bond'
+author = 'Jan Zill, Tyler Pearn, Evelyn Bond'
+release = '1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,15 @@
+.. jaxlogit documentation master file, created by
+   sphinx-quickstart on Mon Dec 15 10:39:26 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+jaxlogit documentation
+======================
+
+jaxlogit is a package used to estimate and predict using mixed logit models.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
