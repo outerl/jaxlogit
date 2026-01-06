639330d03ac53ce959f939941cdc57311e462409
Tyler Pearn <tyler@outerloop.io>
2026-01-06 16:39:00 +1000

    Test bad configurations of variables and correlations for ParametersSetup

--- tests/unit_tests/test__variables.py
+++ tests/unit_tests/test__variables.py
@@ -5,28 +5,41 @@ import numpy as np
 
 from jaxlogit._variables import ParametersSetup
 from jaxlogit._config_data import ConfigData
-from jaxlogit.mixed_logit import MixedLogit
 
 
 def test_wrong_set_variables():
-    missing_set_variable_config = ConfigData(set_vars={"real_variable": 1})
+    missing_set_variable_config = ConfigData(set_vars={"fake_variable": 1})
 
-    with pytest.raises(ValueError, match=r"Variable real_variable not found in the model."):
+    with pytest.raises(ValueError, match=r"Variable fake_variable not found in the model."):
         # rvdist, rvidx, rvidx_normal_bases, rvidx_truncnorm_based, coef_names, betas, config: ConfigData
-        m = MixedLogit()
-
-        m._setup_randvars_info([""], np.array([]))
         ParametersSetup(
             np.array(["n"]),
             jnp.array([True]),
             jnp.array([True]),
             jnp.array([False]),
-            np.array(["fake_variable"]),
+            np.array(["real_variable"]),
             jnp.array([0.1]),
             missing_set_variable_config,
         )
 
 
-if __name__ == "__main__":
-    m = MixedLogit()
-    print(1)
+def test_not_enough_variables_for_correlation():
+    correlation_only_config = ConfigData(include_correlations=True)
+
+    with pytest.raises(ValueError, match=r"Only [01] normal based variable\(s\)\. Cannot use correlation"):
+        # rvdist, rvidx, rvidx_normal_bases, rvidx_truncnorm_based, coef_names, betas, config: ConfigData
+        variable_types_groups = [
+            ["n"],
+            ["trunc_n", "trunc_n", "trunc_n"],
+            ["ln", "trunc_n", "trunc_n"],
+        ]
+        for variable_types in variable_types_groups:
+            ParametersSetup(
+                np.array(variable_types),
+                jnp.array([True for _ in variable_types]),
+                jnp.array([var == "n" or var == "ln" for var in variable_types]),
+                jnp.array([var == "trunc_n" for var in variable_types]),
+                np.array([f"x{i}" for i in enumerate(variable_types)]),
+                jnp.array([0.1 for _ in variable_types]),
+                correlation_only_config,
+            )
