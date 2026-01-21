c67f37c65aa0f8a8832ad34345020cb8830b6ad3
Evelyn Bond <evelyn@outerloop.io>
2026-01-21 16:52:43 +1000

    jit optax

--- jaxlogit/_optimize.py
+++ jaxlogit/_optimize.py
@@ -10,6 +10,8 @@ from scipy.optimize import minimize, OptimizeResult
 from jax.scipy.optimize import minimize as jminimize
 from jax.scipy.optimize import OptimizeResults as OR
 
+from functools import partial
+
 
 logger = logging.getLogger(__name__)
 
@@ -17,22 +19,50 @@ logger = logging.getLogger(__name__)
 STATIC_LOGLIKE_ARGNAMES = ["num_panels", "force_positive_chol_diag", "parameter_info"]
 
 
+def jit_optax_func(loglik_fn, args):
+    (Xdf, Xdr, panels, weights, avail, num_panels, force_positive_chol_diag, draws, parameter_info, batch_size) = args
+
+    @partial(jax.jit, static_argnames=STATIC_LOGLIKE_ARGNAMES)
+    def func(
+        x, *, Xdf, Xdr, panels, weights, avail, draws, num_panels, force_positive_chol_diag, parameter_info, batch_size
+    ):
+        return loglik_fn(
+            x, Xdf, Xdr, panels, weights, avail, num_panels, force_positive_chol_diag, draws, parameter_info, batch_size
+        )
+
+    def wrapper(x):
+        return func(
+            x,
+            Xdf=Xdf,
+            Xdr=Xdr,
+            panels=panels,
+            weights=weights,
+            avail=avail,
+            draws=draws,
+            num_panels=num_panels,
+            force_positive_chol_diag=force_positive_chol_diag,
+            parameter_info=parameter_info,
+            batch_size=batch_size,
+        )
+
+    return wrapper
+
+
 def optax_minimize(loglik_fn, x, args, options, tol):
-    def func(x):
-        return loglik_fn(x, *args)
+    func = jit_optax_func(loglik_fn, args)
+
     solver = optax.lbfgs()
     opt_state = solver.init(x)
     value_and_grad = optax.value_and_grad_from_state(func)
     success = False
     last_x = x
+
     for _ in range(options["maxiter"]):
         value, grad = value_and_grad(x, state=opt_state)
-        updates, opt_state = solver.update(
-            grad, opt_state, x, value=value, grad=grad, value_fn=func
-        )
+        updates, opt_state = solver.update(grad, opt_state, x, value=value, grad=grad, value_fn=func)
         x = optax.apply_updates(x, updates)
-        iter_num = optax.tree.get(opt_state, 'count')
-        grad = optax.tree.get(opt_state, 'grad')
+        iter_num = optax.tree.get(opt_state, "count")
+        grad = optax.tree.get(opt_state, "grad")
         err = optax.tree.norm(grad)
         if err < options["gtol"]:
             success = True
@@ -45,16 +75,24 @@ def optax_minimize(loglik_fn, x, args, options, tol):
 
 
 def scipy_result_to_jax(result: OptimizeResult):
-    return OR(result["x"], result["success"], result["status"], result["fun"],
-              result["jac"], result["hess_inv"], result["nfev"], result["njev"],
-              result["nit"])
+    return OR(
+        result["x"],
+        result["success"],
+        result["status"],
+        result["fun"],
+        result["jac"],
+        result["hess_inv"],
+        result["nfev"],
+        result["njev"],
+        result["nit"],
+    )
 
 
 def _minimize(loglik_fn, x, args, method, tol, options, jit_loglik=True):
     logger.info(f"Running minimization with method {method}")
     neg_loglik_and_grad = loglik_fn
     x = jnp.array(x)
-    
+
     def neg_loglike_scipy(betas, *args):
         """Wrapper for neg_loglike to use with scipy."""
         # x = jnp.array(betas)
@@ -78,7 +116,7 @@ def _minimize(loglik_fn, x, args, method, tol, options, jit_loglik=True):
             tol=tol,
             options=options,
         )
-    elif method == "L-BFGS-B-jaxopt": # Not fully working yet
+    elif method == "L-BFGS-B-jaxopt":  # Not fully working yet
         if jit_loglik:
             neg_loglik_and_grad = jax.jit(
                 jax.value_and_grad(loglik_fn, argnums=0), static_argnames=STATIC_LOGLIKE_ARGNAMES
@@ -91,7 +129,7 @@ def _minimize(loglik_fn, x, args, method, tol, options, jit_loglik=True):
             jnp.full_like(x, -jnp.inf),
             jnp.full_like(x, jnp.inf),
         )
-        
+
         obj = jaxopt.LBFGSB(neg_loglik_and_grad, value_and_grad=True, maxiter=options["maxiter"])
         result = obj.run(x, bounds, *args)
         return result
@@ -105,15 +143,17 @@ def _minimize(loglik_fn, x, args, method, tol, options, jit_loglik=True):
         else:
             # If we are batching, we provide both
             neg_loglik_and_grad = loglik_fn
-        return scipy_result_to_jax(minimize(
-            neg_loglike_scipy,
-            x,
-            args=args,
-            jac=True,
-            method="L-BFGS-B",
-            tol=tol,
-            options=options,
-        ))
+        return scipy_result_to_jax(
+            minimize(
+                neg_loglike_scipy,
+                x,
+                args=args,
+                jac=True,
+                method="L-BFGS-B",
+                tol=tol,
+                options=options,
+            )
+        )
     elif method == "BFGS-scipy":
         if jit_loglik:
             neg_loglik_and_grad = jax.jit(
@@ -122,16 +162,18 @@ def _minimize(loglik_fn, x, args, method, tol, options, jit_loglik=True):
         else:
             # If we are batching, we provide both
             neg_loglik_and_grad = loglik_fn
-        return scipy_result_to_jax(minimize(
-            neg_loglike_scipy,
-            x,
-            args=args,
-            jac=True,
-            method="BFGS",
-            tol=tol,
-            options=options,
-        ))
-        
+        return scipy_result_to_jax(
+            minimize(
+                neg_loglike_scipy,
+                x,
+                args=args,
+                jac=True,
+                method="BFGS",
+                tol=tol,
+                options=options,
+            )
+        )
+
     else:
         logger.error(f"Unknown optimization method: {method} exiting gracefully")
         return None
